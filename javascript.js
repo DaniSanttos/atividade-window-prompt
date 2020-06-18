439f83f01b55c935dad49eec471fa0dee3403fda
Daniel <danisanttt@gmail.com>
2020-06-17 22:14:46 -0300

    adição de palavras complementares

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let n1 = parseInt(window.prompt("Digite um número"))
+let n2 = parseInt(window.prompt("Digite o segundo número"))
+
+if (n1 < n2) {
+    alert ("Com base na sua escolha o " + n2 + " é maior")
+} else {
+    alert ("Com base na sua escolha o " + n1 + " é maior")
+}
